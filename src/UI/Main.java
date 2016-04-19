c2f7ffe50283310f67a6cdb710930770640df048
Buster Kaas Jensen <busterkaas@gmail.com>
2016-04-19 10:41:26 +0200

    first commit

---
+++ src/UI/Main.java
@@ -0,0 +1,15 @@
+package UI;
+
+import Logic.EV3Server;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EV3Server slave = new EV3Server(5000);
+
+	}
+
+}
